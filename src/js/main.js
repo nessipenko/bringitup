1c8cb743f90e909536f647ceb67c5e387714b057
nessipenko <101459146+nessipenko@users.noreply.github.com>
2024-02-23 13:07:53 +0100

    player

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
+import VideoPlayer from "./modules/playVideo";
 import Slider from "./modules/slider";
 
 
 window, addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+    const player = new VideoPlayer('.showup .play', '.overlay')
+    player.init();
 
 
 })
